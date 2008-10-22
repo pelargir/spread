e859cd9c190687b6214fad193aee78169c172a55
Matthew Bass <matt@anacreon.local>
2008-10-22 17:19:29 -0400

    now dynamically generating mailer

--- lib/spread/controller.rb
+++ lib/spread/controller.rb
@@ -1,18 +1,33 @@
 module Spread
   module Controller
-    
     def self.included(base)
       base.extend(ClassMethods)
     end
     
     module ClassMethods
       def spread(action)
+        mailer = construct_mailer
+        construct_action(action, mailer)
+      end
+      
+      def construct_mailer
+        mailer = Class.new(ActionMailer::Base) do
+          def spread(email, url)
+            recipients email
+            body :url => url
+          end
+        end
+        mailer.template_root = "#{RAILS_ROOT}/vendor/plugins/spread/lib/"
+        mailer
+      end
+      
+      def construct_action(action, mailer)
         define_method action do
           if request.post?
             if params[:email].blank?
               flash[:error] = "Email cannot be blank"
             else
-              Spread::Mailer.deliver_spread(params[:email], params[:url])
+              mailer.deliver_spread(params[:email], params[:url])
               flash[:notice] = "An email has been sent to #{params[:email]}"
               redirect_to params[:url] || "/"
             end
@@ -20,7 +35,6 @@ module Spread
         end
       end
     end
-    
   end
 end
 
