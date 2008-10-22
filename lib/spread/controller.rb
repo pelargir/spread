754d5cc7f4e8f980550944f198c04fa469d2048f
Matthew Bass <matt@anacreon.local>
2008-10-22 17:26:11 -0400

    first pass complete with static templates

--- lib/spread/controller.rb
+++ lib/spread/controller.rb
@@ -5,6 +5,7 @@ module Spread
     end
     
     module ClassMethods
+      
       def spread(action)
         mailer = construct_mailer
         construct_action(action, mailer)
@@ -12,12 +13,12 @@ module Spread
       
       def construct_mailer
         mailer = Class.new(ActionMailer::Base) do
-          def spread(email, url)
+          def email(email, url)
             recipients email
             body :url => url
           end
         end
-        mailer.template_root = "#{RAILS_ROOT}/vendor/plugins/spread/lib/"
+        mailer.template_root = "#{RAILS_ROOT}/vendor/plugins/spread/templates/"
         mailer
       end
       
@@ -27,13 +28,15 @@ module Spread
             if params[:email].blank?
               flash[:error] = "Email cannot be blank"
             else
-              mailer.deliver_spread(params[:email], params[:url])
+              mailer.deliver_email(params[:email], params[:url])
               flash[:notice] = "An email has been sent to #{params[:email]}"
-              redirect_to params[:url] || "/"
+              redirect_to params[:url] || "/" and return
             end
           end
+          render :file => "#{RAILS_ROOT}/vendor/plugins/spread/templates/form.html.erb"
         end
       end
+      
     end
   end
 end
