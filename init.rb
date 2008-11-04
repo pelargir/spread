5ffdf4d14a61d9ebfea6f4ff0f63febab26eb8ec
Matthew Bass <matt@anacreon.local>
2008-11-03 22:04:56 -0500

    updated asset_copier

--- init.rb
+++ init.rb
@@ -1,2 +1,9 @@
 require 'spread'
 require 'spread/controller'
+require 'spread/asset_copier'
+
+if RAILS_ENV == "development"
+  Spread::AssetCopier.copy "spread"
+else
+  Spread::AssetCopier.warn "spread"
+end
